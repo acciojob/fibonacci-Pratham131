ed6d87d10a0d30942570ea58676226de6acc51ae
Pratham131 <104488785+Pratham131@users.noreply.github.com>
2023-07-13 09:55:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function fibonacci(num) {
 // your code here
+	let prev = 0;
+	let curr = 1; 
+	// if (num === 1) {
+	// 	console.log(prev);
+	// } else if (num === 2) {
+	// 	console.log(prev);
+	// 	console.log(curr);
+	// }
+	console.log(prev);
+	console.log(curr);
+
+	for (let index = 2; index < num; index++) {
+		let sum = 0 
+		sum = curr + prev;
+		console.log(sum);
+		prev = curr;
+		curr = sum;
+	}
+	
 }
 
 module.exports = fibonacci;
