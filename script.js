0571ac2ba06357378f5e5dda0a7cdfb9de1da2e7
Pratham131 <104488785+Pratham131@users.noreply.github.com>
2023-07-26 17:30:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function fibonacci(num) {
 // your code here
-	if (num === 0) {
+	if (num === 1) {
     return 0;
-  } else if (num === 1 || num === 2) {
+  } else if (num === 2 || num === 3) {
     return 1;
   } else {
     let a = 0;
     let b = 1;
-	for (let index = 1; index <= num-1; index++) {
+	for (let index = 1; index <= num-2; index++) {
 		let temp = b;
 		b = a + b; 
 		a = temp;
